dda636ea33711b1d0a7e678a8905182ed86531b2
José Roberto Mejía Pacehco <isscjrmpacheco@gmail.com>
2024-12-03 10:28:37 -0600

    [UPD] purchase_ext field costo_final

--- extra-addons/purchase_ext/models/models.py
+++ extra-addons/purchase_ext/models/models.py
@@ -14,4 +14,16 @@ class PurchaseOrder(models.Model):
                 if costo_final_factura_lista:
                     costo_final_factura_lista = sum(costo_final_factura_lista) / len(costo_final_factura_lista)
                     # Si la lista no está vacía, obtener el máximo valor
-                    invoice_line.costo_final = costo_final_factura_lista   
+                    invoice_line.costo_final = costo_final_factura_lista
+
+class AccountMoveLine(models.Model):
+    _inherit = 'account.move.line'
+    
+    costo_final = fields.Monetary(string='Costo Final',compute='_compute_costo_final', store=True, currency_field='currency_id')
+    
+    @api.depends('product_id')
+    def _compute_costo_final(self):
+        for line in self:
+            pass
+            #costo_final_factura = line.move_id.purchase_id.order_line.filtered(lambda x: x.product_id.id == line.product_id.id).mapped('x_studio_costo_final')
+            #line.costo_final = costo_final_factura[0] if costo_final_factura else 0                    
